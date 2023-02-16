4b4628ba805d2540d22a5ffa70f9a0c20f4125f1
Xavier Martin <xavierm@axis.com>
2023-02-16 16:44:20 +0100

    save wip
    Change-Id: Icd8bb7b1aec75df3fac59173f088c6fa165e0335

--- img_downloader/src/main.rs
+++ img_downloader/src/main.rs
@@ -7,7 +7,6 @@ use clap::Parser;
 use image;
 use image::DynamicImage;
 use image::GenericImageView;
-use std::fs;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -16,37 +15,6 @@ struct Cli {
     #[arg(default_value_t = 30)]
     fps: u8,
     data_folder: Option<String>,
-}
-
-fn create_folder() -> PathBuf {
-    let default_path = PathBuf::from("data");
-    match default_path.try_exists() {
-        Ok(true) => default_path,
-        Ok(false) => {
-            fs::create_dir(&default_path).ok();
-            println!("Path {:?} will be use as data folder.", &default_path);
-            default_path.clone()
-        }
-        Err(_) => {
-            panic!("OS error, check permission");
-        }
-    }
-}
-
-fn folder_exist(p: &str) -> PathBuf {
-    let path = PathBuf::from(p);
-    match path.try_exists() {
-        Ok(_) => path,
-        Err(_) => create_folder(),
-    }
-}
-
-fn cli_to_config(cli: &Cli) -> MediaConfig {
-    let path = match &cli.data_folder {
-        Some(p) => folder_exist(&p),
-        None => create_folder(),
-    };
-    MediaConfig::new(cli.fps, path)
 }
 
 struct Image {
@@ -68,8 +36,9 @@ impl Image {
 
 fn main() {
     let cli = Cli::parse();
+    let cfg = MediaConfig::new(cli.fps, cli.data_folder);
 
-    let media_handler = MediaHandler::new(cli_to_config(&cli));
+    let media_handler = MediaHandler::new(cfg);
     println!(
         "fps: {:?} | folder path {:?}",
         media_handler.config.fps, media_handler.config.data_folder
--- img_downloader/src/media_handler.rs
+++ img_downloader/src/media_handler.rs
@@ -1,4 +1,6 @@
 use crate::media_config::MediaConfig;
+use std::fs;
+use std::path::PathBuf;
 
 /*
 Add a strategy with a trait to handle different API or local downlloading
@@ -7,10 +9,19 @@ https://rust-unofficial.github.io/patterns/patterns/behavioural/strategy.html
 
 pub struct MediaHandler {
     pub config: MediaConfig,
+    pub media_list: Vec<PathBuf>,
 }
 
 impl MediaHandler {
     pub fn new(config: MediaConfig) -> Self {
-        MediaHandler { config }
+        let ml = fs::read_dir("./").unwrap();
+        MediaHandler {
+            config,
+            media_list: ml,
+        }
     }
+
+    // pub fn get_next_image() -> DynamicImage {
+    //     yield img;
+    // }
 }
